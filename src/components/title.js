37fe90ad1162b335283fb9d13330814cfd332745
Gypsophila。 <futur_wxc@163.com>
2020-08-07 00:23:48 +0800

    feat(组件): title 组件细节调整

--- src/components/title.js
+++ src/components/title.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import Icon from "./icon";
-import { $fontSizeTitle, $fontColorTitle } from "@/style/variables";
+import { $fontSizeTitle, $fontColorTitle, $black } from "@/style/variables";
 
 function Title(props) {
   return (
     <TitleWrapper>
-      <span className="title">{props.children}</span>
-      <Icon size={18} type="arrow-right" iconClass="icon"></Icon>
+      <span>
+        <span className="title">{props.children}</span>
+        <Icon size={18} type="arrow-right" iconClass="icon"></Icon>
+      </span>
     </TitleWrapper>
   );
 }
@@ -17,14 +19,22 @@ export default Title;
 const TitleWrapper = styled.div`
   display: flex;
   padding: 12px 0;
-  cursor: pointer;
 
+  & > span {
+    cursor: pointer;
+    :hover {
+      .title,
+      .icon {
+        color: ${$black};
+      }
+    }
+  }
   .title {
     font-size: ${$fontSizeTitle};
     color: ${$fontColorTitle};
   }
   .icon {
-    margin-left: 4px;
+    padding-left: 4px;
     color: ${$fontColorTitle};
   }
 `;
